44d77f0cb9bb51aa89bf90dcba464a3ae84f6e31
transformerjnm <46799492+transformerjnm@users.noreply.github.com>
2020-03-08 23:15:16 -0400

    star color update
    Made stars that are closer brighter and stars that are farther away darker in the background canvas.

--- js/mobilenav.js
+++ js/mobilenav.js
@@ -5,25 +5,33 @@ let mobile_nav = document.querySelector( '.mobile_nav' );
 
 /*open and close mobile nav when mobile nav logo is clicked*/
 mobile_nav_logo.addEventListener( 'click',
+
 	function ( ) {
+
 		mobile_nav.classList.toggle( 'mobile_nav_active' );
 		mobile_nav_logo.classList.toggle( 'mobile_logo_active' );
 		nav_active = !nav_active;
+
 	}
+
 );
 
+//function that set mobile navigation to inactive
 function remove_mobile_nav_classes( ) {
+
 		mobile_nav.classList.remove( 'mobile_nav_active' );
 			mobile_nav_logo.classList.remove( 'mobile_logo_active' );
 			nav_active = !nav_active;
+			
 }
 
-//if  mobile nav is open and clicked outside of nav. close  mobile nav
+
 let main = document.querySelector( 'main' );
 let header = document.querySelector( '.canvas_container' );
 let herocontent = document.querySelector( '.herocontent' );
 let footer = document.querySelector( 'footer' );
 
+//if  mobile nav is open and clicked outside of nav. close  mobile nav
 if ( nav_active = true ) {
 	/*would not let me select body or header so had to do them individualy*/
 	main.addEventListener( 'click', remove_mobile_nav_classes );
