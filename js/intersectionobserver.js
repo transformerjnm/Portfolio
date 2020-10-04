7df51df68ae12906645388004d3811b051d9b70c
transformerjnm <46799492+transformerjnm@users.noreply.github.com>
2020-10-04 15:23:47 -0400

    added new animations

--- js/intersectionobserver.js
+++ js/intersectionobserver.js
@@ -1,43 +1,60 @@
 /*lazy loading img and img animations*/
 const images = document.querySelectorAll("[lazy-src]");
+const paragraphs = document.querySelectorAll("p");
+const h3 = document.querySelectorAll("h3");
+const h4 = document.querySelectorAll("h4");
+const li = document.querySelectorAll("li");
 
-//load image in. called when close to viewport
-function preload_img(img) {
-	const src = img.getAttribute("lazy-src");
-	if(!src) {
+let preload_img = img => {
+	const lazySrc = img.getAttribute("lazy-src");
+	if(!lazySrc) {
 		return;
 	} else {
-		//set the src of image to lazy src value
-		img.src = src;
+		img.src = lazySrc;
 	}
 }
 
-//loading options. such as how far ahead to load in.
-const img_options = {
+const options = {
     threshold: 0,
     rootMargin: "400px 0px 400px 0px",
 };
 
-//callable image observer. dose not run till called
-const img_observer = new IntersectionObserver((entries, img_observer) => {
+const observer = new IntersectionObserver(entries => {
 	entries.forEach( entry => {
 		//not intersection
 		if (!entry.isIntersecting) {
 			/*remove class for re-animation*/
-			entry.target.classList.remove('showimg');
-			return;
-		}
-		//is intersection with root margin.(near viewport)
-		else {
-			/*show img*/
-			preload_img(entry.target);
-			/*animate img in*/
-			entry.target.classList.add('showimg');
+			if(entry.target.classList.contains('startSlideIn')) {
+				entry.target.classList.remove('startSlideIn');
+			} else if(entry.target.classList.contains('fadeIn')) {
+				entry.target.classList.remove('fadeIn');
+			}
+		} else {
+			/*animate in*/
+			if(entry.target.nodeName === 'IMG') {
+				preload_img(entry.target);
+				entry.target.classList.add('startSlideIn');
+			} else if(entry.target.classList.contains('slideIn')) {
+				entry.target.classList.add('startSlideIn');
+			} else {
+				entry.target.classList.add('fadeIn');
+			}
 		}
 	});
-}, img_options );
+}, options );
 
-//calls the intersection observer on all images on the page for lazy loading
-images.forEach( images => {
-	img_observer.observe( images );
+images.forEach( image => {
+	observer.observe(image);
+});
+h3.forEach( h3 => {
+	observer.observe(h3);
+});
+h4.forEach( h4 => {
+	observer.observe(h4);
+});
+li.forEach( li => {
+	observer.observe(li);
+});
+paragraphs.forEach( p => {
+	observer.observe(p);
 });
