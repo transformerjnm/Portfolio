436b76214e2e81eb3c2be4efdda33ae143663cdb
transformerjnm <46799492+transformerjnm@users.noreply.github.com>
2021-01-07 14:34:35 -0500

    Refactored css variable names and js variable names in canvas.js
    Updated css variable names  to all be camelcase. Also removed unneeded css styles. Updated canvas variable name to camel case also.

--- js/canvas.js
+++ js/canvas.js
@@ -1,41 +1,41 @@
 /* use css to draw background instead of redrawing in canvas every time */
-window.addEventListener( "load", function() {
-    var theCanvas = document.getElementById( 'canvas1' );
+window.addEventListener("load", function() {
+    let theCanvas = document.getElementById( 'starCanvas' );
     //if the canvas created properly
     if(theCanvas && theCanvas.getContext) {
         theCanvas.width = document.documentElement.clientWidth || window.innerWidth;
         theCanvas.height = window.innerHeight;
-        var ctx = theCanvas.getContext( "2d" );
+        let ctx = theCanvas.getContext( "2d" );
         //if we have context of canvas
         if(ctx) {
-            let stars_array = [];
-            stars_array.push(new shooting_star());
-            stars_array.push(new shooting_star());
-            stars_array.push(new shooting_star());
-            stars_array.push(new shooting_star());
-            stars_array.push(new shooting_star());
+            let starsArray = [];
+            starsArray.push(new shootingStar());
+            starsArray.push(new shootingStar());
+            starsArray.push(new shootingStar());
+            starsArray.push(new shootingStar());
+            starsArray.push(new shootingStar());
             /*star object*/
 
-            function shooting_star() {
+            function shootingStar() {
                 //star starting point on canvas
-                this.star_start_x = Math.random() * theCanvas.width;
-                this.star_start_y = Math.random() * theCanvas.height;
+                this.starStartX = Math.random() * theCanvas.width;
+                this.starStartY = Math.random() * theCanvas.height;
                 //size of the circle of the star
-                this.star_size = (Math.random() * 3) + 1;
+                this.starSize = (Math.random() * 3) + 1;
                 //start end is the end of the star's tail
-                this.star_end_x = this.star_start_x + (Math.random() * 100) + 25;
-                this.star_end_y = this.star_start_y - (Math.random() * 50) + 15;
+                this.starEndX = this.starStartX + (Math.random() * 100) + 25;
+                this.starEndY = this.starStartY - (Math.random() * 50) + 15;
             }
 
             function draw(star) {
                /* create star gradient based on size of star. larger stars are closer and brighter. farther stars a dimmer and smaller */
-                var gradient = ctx.createLinearGradient( star.star_start_x, star.star_start_y, star.star_end_x, star.star_end_y );
-                if(star.star_size > 3) {
+                let gradient = ctx.createLinearGradient( star.starStartX, star.starStartY, star.starEndX, star.starEndY );
+                if(star.starSize > 3) {
                    //large bright star
                     gradient.addColorStop("0", "rgba( 165, 199, 227, 1 )");
                     gradient.addColorStop("0.5", "rgba( 165, 199, 227, 1 )");
                     gradient.addColorStop("1.0", "rgba( 165, 199, 227, 0 )");
-                } else if(star.star_size > 2) {
+                } else if(star.starSize > 2) {
                     //medium normal star
                     gradient.addColorStop("0", "rgba( 52, 98, 137, 1 )");
                     gradient.addColorStop("0.5", "rgba( 52, 98, 137, 1 )");
@@ -48,15 +48,15 @@ window.addEventListener( "load", function() {
                 }
                 /*draw the shooting star trail*/
                 ctx.beginPath();
-                ctx.moveTo(star.star_start_x, star.star_start_y);
-                ctx.lineTo(star.star_end_x, star.star_end_y);
+                ctx.moveTo(star.starStartX, star.starStartY);
+                ctx.lineTo(star.starEndX, star.starEndY);
                 ctx.strokeStyle = gradient;
-                ctx.lineWidth = star.star_size;
+                ctx.lineWidth = star.starSize;
                 ctx.stroke();
 
                 /*the star*/
                 ctx.beginPath();
-                ctx.arc(star.star_start_x, star.star_start_y, star.star_size, 0, 2 * Math.PI);
+                ctx.arc(star.starStartX, star.starStartY, star.starSize, 0, 2 * Math.PI);
                 ctx.strokeStyle = "rgba( 125, 220, 255, .20 )";
                 ctx.fillStyle = " white ";
                 ctx.stroke();
@@ -64,18 +64,18 @@ window.addEventListener( "load", function() {
             }
 
             //update star position to make it move
-            function update( star ) {
-                stars_array.forEach( function ( star ) {
+            function update(star) {
+                starsArray.forEach( function ( star ) {
                     let x = 1;
                     let y = 1;
-                    let diff_y = Math.abs( star.star_start_y - star.star_end_y );
-                    let diff_x = Math.abs( star.star_start_x - star.star_end_x );
+                    let diffY = Math.abs( star.starStartY - star.starEndY );
+                    let diffX = Math.abs( star.starStartX - star.starEndX );
                     /*if its negative make it positive*/
-                    if(diff_x < 0) {                     
-                        diff_x = diff_x * diff_x;
+                    if(diffX < 0) {                     
+                        diffX = diffX * diffX;
                     }
-                    if(diff_y < 0) {
-                        diff_y = diff_y * diff_y;
+                    if(diffY < 0) {
+                        diffY = diffY * diffY;
                     }
                     /*
                         based upon where the star trail is relative to the star itself go in the direction that
@@ -84,31 +84,31 @@ window.addEventListener( "load", function() {
                         uses difference so that the star movement speed match the star tail. if the tail
                         is long move faster. Also makes the speed up/down and left/right based upon the tail.
                     */
-                    if(star.star_start_x > star.star_end_x) {
+                    if(star.starStartX > star.starEndX) {
                         /*go right*/
-                        x = (diff_x / 70);
+                        x = (diffX / 70);
                     } else {
                         /*go left*/
-                        x = (-diff_x / 70);
+                        x = (-diffX / 70);
                     }
-                    if(star.star_start_y < star.star_end_y) {
+                    if(star.starStartY < star.starEndY) {
                         /*go up*/
-                        y = (-diff_y / 80);
+                        y = (-diffY / 80);
                     } else {
                         /*go down*/
-                        y = (diff_y / 80);
+                        y = (diffY / 80);
                     }
-                    star.star_start_x = star.star_start_x + x;
-                    star.star_start_y = star.star_start_y + y;
-                    star.star_end_x = star.star_end_x + x;
-                    star.star_end_y = star.star_end_y + y;
+                    star.starStartX = star.starStartX + x;
+                    star.starStartY = star.starStartY + y;
+                    star.starEndX = star.starEndX + x;
+                    star.starEndY = star.starEndY + y;
                     /*if the star is completely out of view. recreate it in view*/
-                    if(star.star_end_x < 0 || star.star_end_y < 0 || star.star_end_x > window.innerWidth  || star.star_end_y > window.innerHeight) {
-                        star.star_start_x = Math.random() * theCanvas.width;
-                        star.star_start_y = Math.random() * theCanvas.height;
-                        star.star_size = (Math.random() * 3 ) + 1;
-                        star.star_end_x = star.star_start_x + (Math.random() * 100) + 25;
-                        star.star_end_y = star.star_start_y - (Math.random() * 50) + 15;   
+                    if(star.starEndX < 0 || star.starEndY < 0 || star.starEndX > window.innerWidth  || star.starEndY > window.innerHeight) {
+                        star.starStartX = Math.random() * theCanvas.width;
+                        star.starStartY = Math.random() * theCanvas.height;
+                        star.starSize = (Math.random() * 3 ) + 1;
+                        star.starEndX = star.starStartX + (Math.random() * 100) + 25;
+                        star.starEndY = star.starStartY - (Math.random() * 50) + 15;   
                     }
                 } );
             }
@@ -123,7 +123,7 @@ window.addEventListener( "load", function() {
                 //reset canvas in-between frames
                 ctx.clearRect(0, 0, theCanvas.width, theCanvas.height);
                 //for every circle re draw it for every frame
-                stars_array.forEach(function(star) {
+                starsArray.forEach(function(star) {
                     draw(star);
                     update(star);
                 });
